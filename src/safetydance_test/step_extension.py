3269ce65aac725f11c63d63ae2670472582fe875
David Charboneau <david@adadabase.com>
2019-12-17 13:06:49 -0500

    Add support for wrapping steps as step_extensions. That way a steps library can be used as-is as well as a step extension.

--- src/safetydance_test/step_extension.py
+++ src/safetydance_test/step_extension.py
@@ -4,7 +4,9 @@ from ast import (
     Load,
     Name,
 )
-from safetydance import step, step_decorator
+from importlib import import_module
+from inspect import getmembers
+from safetydance import step, step_decorator, Step
 from safetydance_test import TestStepPrefix
 from type_extensions import (
     ExtendableType,
@@ -47,11 +49,11 @@ class StepExtension(Extension):
 @step_decorator
 def step_extension(f):
     """
-    Transform a function into a type extension
-    #FIXME figure out how to properly handle class vs instance attrs...
-    What we want is a function f: TestStepPrefix -> (Context ->) *arg -> **kwarg ->
+    Transform a function into a type extension.
+    What we want is a function f: [[TestStepPrefix, Context, *arg, **kwarg], None]
     where f implicitly steals the Context from the calling scope
     """
+    #FIXME figure out how to properly handle class vs instance attrs...
     target_type = TestStepPrefix
     calling_frame = get_calling_frame_as_import()
     if calling_frame is None:
@@ -63,3 +65,29 @@ def step_extension(f):
     f_extension = StepExtension(f)
     target_type.__scoped_setattr__(calling_module, f.__name__, f_extension)
     return f_extension
+
+
+def as_step_extension(step_to_wrap: Step):
+    """
+    Wrap a step as a step_extension for use in testing.
+    """
+    return step_extension(step_to_wrap.f_original)
+
+
+def all_steps_as_step_extensions_from(source_module):
+    """
+    Wrap all steps in the ``source_module`` and add them to the calling module.
+    """
+    newmembers = dict()
+    for k, v in getmembers(
+            source_module,
+            lambda x: isinstance(x, Step) and not isinstance(x, StepExtension)):
+        newmembers[k] = as_step_extension(v)
+    calling_frame = get_calling_frame(not_calling_frame=[__name__])
+    calling_module = calling_frame.f_globals["__name__"]
+    target_module = import_module(calling_module)
+    all_members = []
+    for k, v in newmembers.items():
+        setattr(target_module, k, v)
+        all_members.append(k)
+    return all_members
